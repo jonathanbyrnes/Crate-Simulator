59f8cf01b918ff5924830172d654aaebba10b30f
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-08-01 18:51:45 +0100

    chore: init springboot without postgres details

---
+++ backend/crate-service/src/test/java/core/craft/crateservice/CrateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package core.craft.crateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
