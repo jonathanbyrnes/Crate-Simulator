82d81be16be2b05bf7fba2a7e68c3e3c652c9c3b
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-08-03 18:18:16 +0100

    fix: ensure CreateCrateRequest can be instantiated with all variables

--- backend/crate-service/src/main/java/core/craft/crateservice/dto/CreateCrateRequest.java
+++ backend/crate-service/src/main/java/core/craft/crateservice/dto/CreateCrateRequest.java
@@ -3,6 +3,7 @@ package core.craft.crateservice.dto;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CreateCrateRequest {
 
     @NotBlank(message = "A crate name is required.")
