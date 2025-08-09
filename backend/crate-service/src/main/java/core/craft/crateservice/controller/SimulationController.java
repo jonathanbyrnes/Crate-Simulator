82d05b817a687ac275b8e174168fed77017cf2da
BluntPikachu <48105542+jonathanbyrnes@users.noreply.github.com>
2025-08-09 16:47:57 +0100

    test: add Unit tests for SimulationController endpoint & update OpeningController endpoint (+test update)

--- backend/crate-service/src/main/java/core/craft/crateservice/controller/SimulationController.java
+++ backend/crate-service/src/main/java/core/craft/crateservice/controller/SimulationController.java
@@ -1,6 +1,6 @@
 package core.craft.crateservice.controller;
 
-import core.craft.crateservice.service.SimulationService;
+import core.craft.crateservice.service.OpeningRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class SimulationController {
 
-    private final SimulationService service;
+    private final OpeningRequestService service;
 
     @PostMapping
     public ResponseEntity<Void> simulate(@PathVariable Long crateId,
                                          @RequestParam(defaultValue = "1000") int count) {
-        service.simulate(crateId, count);
+        service.requestOpenings(crateId, count);
         return ResponseEntity.accepted().build();
     }
 }
