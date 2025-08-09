e77040eacfab36e33af9c107d8f7def6a4ada807
BluntPikachu <48105542+jonathanbyrnes@users.noreply.github.com>
2025-08-09 16:07:52 +0100

    refactor: fixed intellij formatting

--- backend/crate-service/src/main/java/core/craft/crateservice/controller/OpeningController.java
+++ backend/crate-service/src/main/java/core/craft/crateservice/controller/OpeningController.java
@@ -1,24 +1,24 @@
-    package core.craft.crateservice.controller;
+package core.craft.crateservice.controller;
 
-    import core.craft.crateservice.dto.CreateOpeningRequest;
-    import core.craft.crateservice.dto.OpeningDto;
-    import core.craft.crateservice.publisher.OpeningPublisher;
-    import lombok.RequiredArgsConstructor;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.web.bind.annotation.*;
+import core.craft.crateservice.dto.CreateOpeningRequest;
+import core.craft.crateservice.dto.OpeningDto;
+import core.craft.crateservice.publisher.OpeningPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-    @RestController
-    @RequestMapping("/api/crates/{crateId}/open")
-    @RequiredArgsConstructor
-    public class OpeningController {
+@RestController
+@RequestMapping("/api/crates/{crateId}/open")
+@RequiredArgsConstructor
+public class OpeningController {
 
-        private final OpeningPublisher publisher;
+    private final OpeningPublisher publisher;
 
-        @PostMapping
-        public ResponseEntity<OpeningDto> open(@PathVariable Long crateId) {
-            publisher.publishOpening(new CreateOpeningRequest(crateId));
-            return ResponseEntity.accepted().build();
-        }
-
+    @PostMapping
+    public ResponseEntity<OpeningDto> open(@PathVariable Long crateId) {
+        publisher.publishOpening(new CreateOpeningRequest(crateId));
+        return ResponseEntity.accepted().build();
     }
+
+}
 
